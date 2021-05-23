34e381b172a48cac936b0749dc7aed00183ae6d9
Shiva2711 <shiva.rcse@gmail.com>
2021-05-23 23:31:11 +0530

    Extent Report Practise

---
+++ src/test/java/com/Practice/extentReport.java
@@ -0,0 +1,50 @@
+package com.Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class extentReport {
+	ExtentReports extent;
+
+	@BeforeTest
+	public void before() {
+		
+		String path = System.getProperty("user.dir") + "//reports//pratice.html";
+		
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Result");
+		reporter.config().setDocumentTitle("Test Result");
+		reporter.config().setTheme(Theme.DARK);
+		
+		extent = new ExtentReports();
+		
+		extent.attachReporter(reporter);
+		
+		extent.setSystemInfo("Tester", "Siva Chandran R");
+
+	}
+	
+	@Test
+	public void test() {
+		
+		ExtentTest test = extent.createTest("Intial Demo");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//lib//chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("hhtps://www.rummybazzi.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result do not match");
+		
+		extent.flush();
+	}
+
+}
