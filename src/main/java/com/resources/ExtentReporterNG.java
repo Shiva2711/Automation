c0d456d39fc3278cfd00db3f21c3a435e5e0650f
Shiva2711 <shiva.rcse@gmail.com>
2021-05-27 15:00:17 +0530

    Extent Reporter - Dark Theme

--- src/main/java/com/resources/ExtentReporterNG.java
+++ src/main/java/com/resources/ExtentReporterNG.java
@@ -2,6 +2,7 @@ package com.resources;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReporterNG {
 	static ExtentReports  extent;
@@ -11,6 +12,7 @@ public class ExtentReporterNG {
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 		reporter.config().setReportName("Web Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
+		reporter.config().setTheme(Theme.DARK);
 		extent =new ExtentReports();
 		extent.attachReporter(reporter);
 		extent.setSystemInfo("Tester", "Siva Chandran R");
