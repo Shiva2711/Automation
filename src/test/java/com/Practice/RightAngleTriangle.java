79597ee83eeca718a453985dbc6dc1268e05aaf6
Shiva2711 <shiva.rcse@gmail.com>
2025-11-21 22:20:44 +0530

    New

--- src/test/java/com/Practice/RightAngleTriangle.java
+++ src/test/java/com/Practice/RightAngleTriangle.java
@@ -3,7 +3,7 @@ public class RightAngleTriangle {
     public static void main(String[] args) {
         int rows = 5;  // Number of rows in the triangle
 
-        for (int i = 1; i <= rows; i++) {
+        for (int i = 1; i <= rows; i++) { 
             // Print stars for each row
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
